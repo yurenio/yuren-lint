9a2fa5acede3beb40794a3357387d002807417d3
WenryXu <wenryxu@outlook.com>
2017-11-19 14:47:00 +0800

    chore(gulp): update comment

--- gulpfile.js
+++ gulpfile.js
@@ -14,22 +14,23 @@ gulp.task('html', () => {
         gulp.src('./*.html')
             // HTMLLint
             .pipe(htmlLint({
-                htmllintrc: "./.htmllintrc",
+                htmllintrc: "./.htmllintrc", // 使用 `.htmllintrc` 文件的配置
                 useHtmllintrc: true
             }))
             .pipe(htmlLint.format())
-            .pipe(htmlLint.failOnError())
+            .pipe(htmlLint.failOnError()) // 出现 Error 时中断 Gulp Task
     )
 })
 
 gulp.task('css', () => {
     return es.concat(
+        // CSSLint
         gulp.src('./src/css/*.css')
             .pipe(csslint('csslintrc.json'))
             .pipe(csslint.formatter()),
-        gulp.src('./src/css/*.css')
+        gulp.src('./src/css/*.css') // `formatter()` 与 `failFormatter()` 同时存在时，不会输出 Error 内容
             .pipe(csslint('csslintrc.json'))
-            .pipe(csslint.failFormatter())
+            .pipe(csslint.failFormatter()) // 出现 Error 时中断 Gulp Task
             .pipe(gulp.dest('./dest/'))
     )
 })
